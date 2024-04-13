42c650feace7bbf1997a6cfcc923ed5043f6f50b
Harshitk15 <139445006+Harshitk15@users.noreply.github.com>
2024-04-13 18:02:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { SpeechProvider } from "@speechly/react-client";
 import { Provider } from "./context/context";
 import './index.css';
 ReactDOM.render(
-  <SpeechProvider appId="f62f927d-0728-4495-a7fa-75ef28ed0984" language="en-US">
+  
   <Provider>
     <App/>
   </Provider>
-  </SpeechProvider>,
+ ,
   document.getElementById('root'));
